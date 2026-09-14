23782bf5416b9378a767c2514220de31a05b473a
Ashimolowo <ashimolowoayobami@gmail.com>
2026-09-14 06:22:16 -0700

    using express Router to simplify the routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import reminderRoutes from './routes/remindersRoutes.js'
 
 const app = express()
 const port = process.env.PORT
@@ -7,29 +8,7 @@ app.get('/', (req, res) => {
     res.send('Hello Ridwan')
 })
 
-app.get('/reminders', (req, res) => {
-    res.send('Get all reminders');
-})
-
-app.get('/reminders/:id', (req, res) => {
-    res.send('Get single reminder by id')
-})
-
-app.post('/reminders', (req, res) => {
-    res.send('Create a new reminder')
-})
-
-app.patch('/reminders/:id', (req, res) => {
-    res.send('Update some fields for existing reminder')
-})
-
-app.delete('/reminders/:id', (req, res) => {
-    res.send('Delete a reminder')
-})
-
-app.delete("/reminders", (req, res) => {
-  res.send("Delete all reminder");
-});
+app.use('/reminders', reminderRoutes)
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
