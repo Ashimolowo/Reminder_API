7f35ee6721f411eb0deb1c79c6ac69fbdf5fd458
Ashimolowo <ashimolowoayobami@gmail.com>
2026-09-14 05:41:33 -0700

    Simple routing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ app.get('/', (req, res) => {
     res.send('Hello Ridwan')
 })
 
+app.get('/reminders', (req, res) => {
+    res.send('Get all reminders');
+})
+
+app.get('/reminders/:id', (req, res) => {
+    res.send('Get single reminder by id')
+})
+
+app.post('/reminders', (req, res) => {
+    res.send('Create a new reminder')
+})
+
+app.patch('/reminders/:id', (req, res) => {
+    res.send('Update some fields for existing reminder')
+})
+
+app.delete('/reminders/:id', (req, res) => {
+    res.send('Delete a reminder')
+})
+
+app.delete("/reminders", (req, res) => {
+  res.send("Delete all reminder");
+});
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
