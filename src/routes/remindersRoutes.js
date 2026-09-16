7945028606e7dfa714702e308b61a26eecf66c6a
Ashimolowo <ashimolowoayobami@gmail.com>
2026-09-16 06:15:16 -0700

    Validated schemas and handle the middleware

--- src/routes/remindersRoutes.js
+++ src/routes/remindersRoutes.js
@@ -1,19 +1,34 @@
 import { Router } from "express";
 import { ReminderController } from "../controllers/reminderController.js";
+import { validateData } from "../middlewares/validationMiddleware.js";
+import {
+  createReminderSchema,
+  updateReminderSchema,
+} from "../schemas/reminderSchema.js";
 
 const router = Router();
 
 
 router.get("/", ReminderController.getAllReminders);
 
+router.delete("/", ReminderController.deleteAllReminders);
+
 router.get("/:id", ReminderController.getRemindersById);
 
-router.post("/", ReminderController.createReminder);
+router.post(
+  "/",
+  validateData(createReminderSchema),
+  ReminderController.createReminder,
+);
 
-router.patch("/:id", ReminderController.updateReminder);
+
+router.put(
+  "/:id",
+  validateData(updateReminderSchema),
+  ReminderController.updateReminder,
+);
+
 
 router.delete("/:id", ReminderController.deleteReminder);
-
-router.delete("/", ReminderController.deleteAllReminders);
 
 export default router;
